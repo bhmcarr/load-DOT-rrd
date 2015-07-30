f8abd49dfea5256c0b800670dfeaaa8231e45bea
Brandon Carr <bcarr@synacor.com>
2015-07-30 15:10:40 -0400

    FIXED THE THING

--- rrds_hourly_cpu.pl
+++ rrds_hourly_cpu.pl
@@ -12,12 +12,12 @@ my $rrd_graph = "public/graph/load_graph.png";
 if(-f "load.rrd"){
 	unlink "load.rrd" or print "Unable to delete load.rrd: $!";
 }
-RRDs::create($rrd, "-s 20", "DS:load:GAUGE:60:0:8", "RRA:LAST:0.5:12:24");
+RRDs::create($rrd, "-s 60", "DS:load:GAUGE:120:0:8", "RRA:AVERAGE:0.9:1:60");
 
 $value = 0;
 while(){
 	my $cur_time = time();
-	my $end_time = $cur_time - 60;
+	my $end_time = $cur_time;
 	my $start_time = $end_time - 1800;
 	open (STAT,"/proc/loadavg") or die "Cannot open /proc/loadavg";
 	while(<STAT>){
@@ -26,7 +26,7 @@ while(){
 	}
 
 	#$value = $cpu[0];
-	RRDs::update($rrd, "N:$value");
+	RRDs::update($rrd, "N:$value") or die "Cannot update RRD";
 	my $err=RRDs::error;
 	print "$err\n" if $err;
 
@@ -36,18 +36,18 @@ while(){
 
 	RRDs::graph("$rrd_graph",
 				"--width", "710",
-				"--height" , "200",
+				"--height" , "1000",
 				"--start", "$start_time",
 				"--end", "$end_time",
 				"--lower-limit", "0",
 				"--upper-limit", "8",
 				"--title= CPU Load",
 				"--vertical-label= load value",
-				"DEF:load=$rrd:load:LAST",
-				"LINE1:load#FF0000:Load",
-				"GPRINT:load:MIN:\"%6.2lf min\l\"",
-				"GPRINT:load:MAX:\"%6.2lf max\l\"",
-				"GPRINT:load:AVERAGE:\"%6.2lf average\l\"");
-	print ("Updated RRD with value:$value\n");
-	sleep 20;
+				"DEF:load=$rrd:load:AVERAGE",
+				"LINE1:load#FF0000:load",
+				"GPRINT:load:MIN: %6.2lf min",
+				"GPRINT:load:MAX: %6.2lf max",
+				"GPRINT:load:AVERAGE: %6.2lf average");
+			#print ("Updated RRD with value:$value\n");
+	sleep 60;
 }
